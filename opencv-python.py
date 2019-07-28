532efecc57928d73a59e516c4f5f670350bfe66f
Kazuhei Hamada <hamada.kazuhei@gmail.com>
2019-07-28 19:53:11 +0900

    Add a argument to specify the image to process

--- opencv-python.py
+++ opencv-python.py
@@ -1,11 +1,14 @@
+import sys
 import numpy as np
 import cv2
 
+args = sys.argv
+
+img  = cv2.imread(args[1])
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 eye_cascade  = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
-
-img  = cv2.imread('kzone_Fuji.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
