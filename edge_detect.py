b13ad85472c01ee150c95cbde391928169d61aaf
Kazuhei Hamada <hamada.kazuhei@gmail.com>
2019-07-29 20:30:34 +0900

    Add detection of edge function

--- edge_detect.py
+++ edge_detect.py
@@ -11,18 +11,18 @@ imgYUV = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
 
 imgY = imgYUV[:,:,0]
 
-result = cv2.Laplacian(imgY, cv2.CV_64F)
-
-cv2.imshow('img', result)
-
+laplacian = cv2.Laplacian(imgY, cv2.CV_64F)
+canny = cv2.Canny(imgY, 100, 200)
 dx = cv2.Sobel(imgY, cv2.CV_64F, 1, 0, ksize=3)
 dy = cv2.Sobel(imgY, cv2.CV_64F, 0, 1, ksize=3)
 grad = np.sqrt(dx ** 2 + dy ** 2)
 
 cv2.imshow('imgY', imgY)
+cv2.imshow('laplacian', laplacian)
 cv2.imshow('dx', dx)
 cv2.imshow('dy', dy)
 cv2.imshow('grad', grad)
+cv2.imshow('canny', canny)
 
 cv2.waitKey(0)
 
